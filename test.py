0a7729a32b9372e159092d1b28434e52e059d43b
Yaru Cui <sisucyrcyr@gmail.com>
2026-01-24 22:37:05 +0100

    finished MiNiGPT

--- test.py
+++ test.py
@@ -18,22 +18,27 @@ import torch
 # val_data = data[n:]
 # vocab_size = len(chars)
 
-def test_get_batch():
-    data = torch.arange(20)
-    block_size = 5
-    batch_size = 3
+# def test_get_batch():
+#     data = torch.arange(20)
+#     block_size = 5
+#     batch_size = 3
 
 
-    ix = torch.randint(len(data) - block_size - 1, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+#     ix = torch.randint(len(data) - block_size - 1, (batch_size,))
+#     x = torch.stack([data[i:i+block_size] for i in ix])
+#     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
 
-    print("data:", data)
-    print("ix:", ix)
-    print("x shape:", x.shape)
-    print("y shape:", y.shape)
-    print("x:")
-    print(x)
-    print("y")
+#     print("data:", data)
+#     print("ix:", ix)
+#     print("x shape:", x.shape)
+#     print("y shape:", y.shape)
+#     print("x:")
+#     print(x)
+#     print("y")
 
-test_get_batch()
+# test_get_batch()
+
+x = torch.randint(10, (2,10,5))
+y = x.view(-1, x.size(-1))
+print(x)
+print(y)
